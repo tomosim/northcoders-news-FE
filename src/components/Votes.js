1b0324313210a7fc08fab3a645272861c1359d5d
Tom <TomSimmons@Toms-MacBook-Pro.local>
2018-07-13 09:47:33 +0100

    imporoved funtionality of up/down vote buttons. The vote numbers now toggle

--- src/components/Votes.js
+++ src/components/Votes.js
@@ -1,10 +1,34 @@
 import React, { Component } from "react";
 
 class Votes extends Component {
-  state = { voted: "" };
+  state = { votes: { up: 0, down: 0 } };
 
   handleVote = (article_id, dir) => {
-    this.props.updateVotes(article_id, dir, this.props.id);
+    if (dir === "up") {
+      if (this.state.votes.up === 0 && this.state.votes.down === 1) {
+        this.props.updateVotes(article_id, dir, this.props.id);
+        this.props.updateVotes(article_id, dir, this.props.id);
+        this.setState({ votes: { up: 1, down: 0 } });
+      } else if (this.state.votes.up === 0) {
+        this.props.updateVotes(article_id, dir, this.props.id);
+        this.setState({ votes: { up: 1, down: 0 } });
+      } else {
+        this.props.updateVotes(article_id, "down", this.props.id);
+        this.setState({ votes: { up: 0, down: 0 } });
+      }
+    } else {
+      if (this.state.votes.down === 0 && this.state.votes.up === 1) {
+        this.props.updateVotes(article_id, dir, this.props.id);
+        this.props.updateVotes(article_id, dir, this.props.id);
+        this.setState({ votes: { up: 0, down: 1 } });
+      } else if (this.state.votes.down === 0) {
+        this.props.updateVotes(article_id, dir, this.props.id);
+        this.setState({ votes: { up: 0, down: 1 } });
+      } else {
+        this.props.updateVotes(article_id, "up", this.props.id);
+        this.setState({ votes: { up: 0, down: 0 } });
+      }
+    }
   };
 
   render() {
